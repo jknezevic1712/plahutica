2bb78bb15c1b9b00c48fa0a8d09561808834b22b
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-07-19 08:20:07 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./pages/header/header";
+import Home from "./pages/home/home";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
